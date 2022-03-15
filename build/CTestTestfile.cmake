72726c5f0b4ea2e92ce1632effa4232ebff0c233
MuriloGhignatti <muriloghignatti@gmail.com>
2022-03-14 21:13:23 -0300

    Added the cmake build debug folder and removed unecessary directories

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/murilo/Desktop/Codes/C++/Graph
-# Build directory: /home/murilo/Desktop/Codes/C++/Graph/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
